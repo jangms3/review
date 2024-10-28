ea6becfadf0231c5649b981e52bfb39630270227
jangms3 <github.com/jangms3>
2024-10-28 09:59:24 +0900

    @OneToOne 양방향 및 조회 테스트

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -13,4 +13,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @OneToOne(mappedBy = "user")
+    private Food food;
+
+    public void addFood(Food food) {
+        this.food = food;
+        food.setUser(this); //this 자기 자체 객체 // 객체 자신을 자기 자신에게 주는 건데 필드로 외래키를 설정?
+    }
 }
