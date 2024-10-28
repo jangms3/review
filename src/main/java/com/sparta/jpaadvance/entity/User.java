1b38399d4fc107d249f677512dc7fce5f587e1b1
jangms3 <github.com/jangms3>
2024-10-28 19:42:20 +0900

    영속성 전이

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -19,6 +19,13 @@ public class User {
 
 
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user",cascade = CascadeType.REMOVE)
+    // food 까지 한번에 저장되길 원해 cascade type 중 persist 사용
+    // delete 는 remove 지원
     private List<Food> foodList =new ArrayList<>();
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.setUser(this); // 외래 키 설정
+    }
 }
