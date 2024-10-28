4503a49d29ed5c0208e0af72bff1d4a29bef58fd
jangms3 <github.com/jangms3>
2024-10-28 19:55:02 +0900

    Orphan Removal 적용

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -19,9 +19,12 @@ public class User {
 
 
 
-    @OneToMany(mappedBy = "user",cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "user",cascade = CascadeType.PERSIST, orphanRemoval = true)
     // food 까지 한번에 저장되길 원해 cascade type 중 persist 사용
     // delete 는 remove 지원
+
+    //jpa 상에서 orphan  removal =true 를 지원함으로 써
+    // 연관관계 뿐만 아니라 db에서도 데이터 처리가 가능해짐
     private List<Food> foodList =new ArrayList<>();
 
     public void addFoodList(Food food) {
